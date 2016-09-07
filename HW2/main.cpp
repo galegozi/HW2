f898a613dd8fcf4cbb80aac76a9d41c7e37026e4
Gal Egozi <gegozi@comcast.net>
2016-09-06 22:05:57 -0500

    Initial Commit

---
+++ HW2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW2
+//
+//  Created by Gal Egozi 1 on 9/6/16.
+//  Copyright © 2016 Gal Egozi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
