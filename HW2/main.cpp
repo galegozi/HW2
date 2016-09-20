a9523dea2b9b3a34527e7144043c02bfa21f399d
githubgal <geegozi@gmail.com>
2016-09-20 12:56:24 -0500

    A homework Assignment.
    Cosc 1430

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  HW2
-//
-//  Created by Gal Egozi 1 on 9/6/16.
-//  Copyright © 2016 Gal Egozi. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    int * a = new int;
+    //int * c = new int;
+    for(int x = 0; x < 20; x++)
+    {
+        a[x] = x;
+        cout << "a[" << x<< "] = " << a[x] << endl;
+    }
+    int * b;
+    b = a;
+    for(int x = 0; x < 20; x++)
+    {
+        cout << "b["<< x << "] = " << b[x] << endl;
+    }
+    a = new int[20];
+    for(int x = 0; x < 20; x++)
+    {
+        a[x] = 20+x;
+        cout << "a["<< x << "] = " << a[x] << endl;
+    }
+    for(int x = 0; x < 20; x++)
+    {
+        cout << "b["<< x << "] = " << b[x] << endl;
+    }
+    delete a;
+    delete b;
     return 0;
 }
